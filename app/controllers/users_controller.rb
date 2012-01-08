dc3d8e6cf6fb2e14a63a36453d9a1680a1e3a32e
Marianna Milesi <mm00128@surrey.ac.uk>
2012-01-08 03:29:37 +0000

    now can destroy users as admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 before_filter :authenticate, :only => [:edit, :update]
-
+before_filter :correct_user, :only => [:edit, :update]
+before_filter :admin_user,   :only => :destroy
 
   def show
     @user = User.find(params[:id])
@@ -55,6 +56,11 @@ before_filter :authenticate, :only => [:edit, :update]
     @users = User.paginate(:page => params[:page])
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
 
 private
 
@@ -67,6 +73,9 @@ private
       redirect_to(root_path) unless current_user?(@user)
     end
 
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
 
 
